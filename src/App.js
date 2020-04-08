d7d80bba333bb7125862bc5097cb80ecff1c1d70
Akshay Gulhane <akshay.gulhane@opcito.com>
2020-04-08 15:14:15 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+const Home = require('./components/Home');
+const Rooms = require('./components/Home');
+const Times = require('./components/Home');
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Route exact path="/" component={Home} />
+        <Route path="/rooms" component={Rooms} />
+        <Route path="/times" component={Times} />
+      </div>
+    </Router>
   );
 }
 
